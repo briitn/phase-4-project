4c434229eb17365aa4165d8afbe9fc5b1b5ba064
princegobah <babynamek@gmail.com>
2023-01-12 06:16:39 -0500

    fecth to login

--- client/src/Login.js
+++ client/src/Login.js
@@ -48,7 +48,7 @@ function Login({setUserStuff}){
     function changeUser(e){
 
         setUsername(e.target.value)
-        console.log(username)
+      
     }
     return (
         <div className='fox'>
