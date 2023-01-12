f332307265d830d6c5972320cf9c3cd60cb5b531
princegobah <babynamek@gmail.com>
2023-01-12 07:07:15 -0500

    delete account feature

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,6 +1,6 @@
 
 import {Fragment, useEffect, useState} from 'react'
-import { Redirect } from 'react-router-dom'
+
 import { useHistory } from 'react-router-dom'
  import CreateTweets from './CreateTweets'
 
